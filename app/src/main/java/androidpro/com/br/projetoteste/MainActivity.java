2cdbd120e6a2dad58c079f0fba93bafb9fa51b03
adilsonresende <adilsonresendetp@gmail.com>
2016-09-25 11:33:16 -0300

    Criação de variável global Teste.

--- app/src/main/java/androidpro/com/br/projetoteste/MainActivity.java
+++ app/src/main/java/androidpro/com/br/projetoteste/MainActivity.java
@@ -6,11 +6,13 @@ import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+String teste = "teste";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     public void mensagem(View view){
